c5a73e09389740752771a67c8de9723b4c0ca477
ladyada <limor@ladyada.net>
2016-12-29 16:50:50 -0500

    fix bad memcpy

--- src/platform/src/vl53l0x_i2c_comms.cpp
+++ src/platform/src/vl53l0x_i2c_comms.cpp
@@ -56,13 +56,19 @@ int VL53L0X_write_byte(uint8_t deviceAddress, uint8_t index, uint8_t data) {
 
 int VL53L0X_write_word(uint8_t deviceAddress, uint8_t index, uint16_t data) {
   uint8_t buff[2];
-  memcpy(buff, data, 2);
+  buff[1] = data & 0xFF;
+  buff[0] = data >> 8;
   return VL53L0X_write_multi(deviceAddress, index, buff, 2);
 }
 
 int VL53L0X_write_dword(uint8_t deviceAddress, uint8_t index, uint32_t data) {
   uint8_t buff[4];
-  memcpy(buff, data, 4);
+
+  buff[3] = data & 0xFF;
+  buff[2] = data >> 8;
+  buff[1] = data >> 16;
+  buff[0] = data >> 24;
+
   return VL53L0X_write_multi(deviceAddress, index, buff, 4);
 }
 
@@ -71,15 +77,32 @@ int VL53L0X_read_byte(uint8_t deviceAddress, uint8_t index, uint8_t *data) {
 }
 
 int VL53L0X_read_word(uint8_t deviceAddress, uint8_t index, uint16_t *data) {
-  uint8_t buffer[2];
-  int r = VL53L0X_read_multi(deviceAddress, index, buffer, 2);
-  memcpy(data, buffer, 2);
+  uint8_t buff[2];
+  int r = VL53L0X_read_multi(deviceAddress, index, buff, 2);
+
+  uint16_t tmp;
+  tmp = buff[0];
+  tmp <<= 8;
+  tmp |= buff[1];
+  *data = tmp;
+
   return r;
 }
 
 int VL53L0X_read_dword(uint8_t deviceAddress, uint8_t index, uint32_t *data) {
-  uint8_t buffer[4];
-  int r = VL53L0X_read_multi(deviceAddress, index, buffer, 4);
-  memcpy(data, buffer, 4);
+  uint8_t buff[4];
+  int r = VL53L0X_read_multi(deviceAddress, index, buff, 4);
+
+  uint32_t tmp;
+  tmp = buff[0];
+  tmp <<= 8;
+  tmp |= buff[1];
+  tmp <<= 8;
+  tmp |= buff[2];
+  tmp <<= 8;
+  tmp |= buff[3];
+
+  *data = tmp;
+
   return r;
 }
