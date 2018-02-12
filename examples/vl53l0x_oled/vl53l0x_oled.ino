ee901b5a546c7899c14668f80cfeb8f3b73f1b3d
dean <deanm1278@gmail.com>
2018-02-12 17:05:20 -0500

    DM: fix OLED demo

--- examples/vl53l0x_oled/vl53l0x_oled.ino
+++ examples/vl53l0x_oled/vl53l0x_oled.ino
@@ -1,18 +1,17 @@
-/* This example shows how to use continuous mode to take
-range measurements with the VL53L0X. It is based on
-vl53l0x_ContinuousRanging_Example.c from the VL53L0X API.
+/* This example shows how to take
+range measurements with the VL53L0X and display on a SSD1306 OLED.
 
 The range readings are in units of mm. */
 
 #include <Wire.h>
-#include <VL53L0X.h>
+#include "Adafruit_VL53L0X.h"
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 Adafruit_SSD1306 display = Adafruit_SSD1306();
 
-VL53L0X sensor;
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 #if (SSD1306_LCDHEIGHT != 32)
  #error("Height incorrect, please fix Adafruit_SSD1306.h!");
@@ -30,14 +29,10 @@ void setup()
   
   Wire.begin();
 
-  sensor.init();
-  sensor.setTimeout(500);
-
-  // Start continuous back-to-back mode (take readings as
-  // fast as possible).  To use continuous timed mode
-  // instead, provide a desired inter-measurement period in
-  // ms (e.g. sensor.startContinuous(100)).
-  sensor.startContinuous();
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
 
   // text display big!
   display.setTextSize(4);
@@ -46,22 +41,21 @@ void setup()
 
 void loop()
 {
-  uint16_t reading = sensor.readRangeContinuousMillimeters();
+  VL53L0X_RangingMeasurementData_t measure;
 
-  if (reading > 8000) {
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+      display.clearDisplay();
+      display.setCursor(0,0);
+      display.print(measure.RangeMilliMeter);
+      display.print("mm");
+      display.display();
+      Serial.println();
+      delay(50);
+  } else {
     display.display();
     display.clearDisplay();
     return;
   }
-  
-  Serial.print(sensor.readRangeContinuousMillimeters());
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
-
-  display.clearDisplay();
-  display.setCursor(0,0);
-  display.print(reading);
-  display.print("mm");
-  display.display();
-  Serial.println();
-  delay(50);
 }
