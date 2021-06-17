401539c7027fc9c1b1121c8ad29612e3928696c3
Abhijeet  kr <80936610+4BH1J337@users.noreply.github.com>
2021-06-17 07:00:57 +0530

    Update vl53l0x.ino
    Updated VL53L0X  sensor basic program to eliminate Failed to boot error!

--- examples/vl53l0x/vl53l0x.ino
+++ examples/vl53l0x/vl53l0x.ino
@@ -3,34 +3,46 @@
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 void setup() {
-  Serial.begin(115200);
+  lox.begin(0x2B);// put any address between 0x29 to 0x7F
+  Serial.begin(9600);
 
   // wait until serial port opens for native USB devices
   while (! Serial) {
     delay(1);
   }
-  
+
   Serial.println("Adafruit VL53L0X test");
   if (!lox.begin()) {
     Serial.println(F("Failed to boot VL53L0X"));
-    while(1);
+    while (1);
   }
-  // power 
-  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+  // power
+  Serial.println(F("VL53L0X API Simple Ranging example\n\n"));
 }
 
 
 void loop() {
   VL53L0X_RangingMeasurementData_t measure;
-    
+
   Serial.print("Reading a measurement... ");
   lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
   if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+    Serial.print("Distance (mm): ");
+    Serial.println(measure.RangeMilliMeter);
+
+    //  for centimeter remove comments
+
+    //    int centimeter;
+    //    centimeter = measure.RangeMilliMeter / 10; // convetrint mm into Cm
+    //    Serial.print("  , Distance (Cm): ");
+    //    Serial.print(centimeter);
+    //    Serial.println(" ");
+
+
   } else {
     Serial.println(" out of range ");
   }
-    
-  delay(100);
+
+  delay(200); //little delay to improve simulation performance
 }
