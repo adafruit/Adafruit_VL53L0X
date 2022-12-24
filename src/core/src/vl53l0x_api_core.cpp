1cc9fa904985cb80d61c7aaec8c412f05146f2f7
Phillip Burgess <paintyourdragon@dslextreme.com>
2022-12-23 17:57:27 -0800

    clang-format vl53l0x_api_core.cpp

--- src/core/src/vl53l0x_api_core.cpp
+++ src/core/src/vl53l0x_api_core.cpp
@@ -490,8 +490,9 @@ uint32_t VL53L0X_calc_timeout_mclks(VL53L0X_DEV Dev, uint32_t timeout_period_us,
   macro_period_ps = VL53L0X_calc_macro_period_ps(Dev, vcsel_period_pclks);
   macro_period_ns = (macro_period_ps + 500) / 1000;
 
-  timeout_period_mclks = (uint32_t)(
-      ((timeout_period_us * 1000) + (macro_period_ns / 2)) / macro_period_ns);
+  timeout_period_mclks =
+      (uint32_t)(((timeout_period_us * 1000) + (macro_period_ns / 2)) /
+                 macro_period_ns);
 
   return timeout_period_mclks;
 }
