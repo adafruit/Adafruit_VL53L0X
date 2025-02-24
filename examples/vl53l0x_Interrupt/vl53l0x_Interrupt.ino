d40eaff53e76706dc5765cb61a6873cfc0882817
Rick Wargo <rickwargo@users.noreply.github.com>
2025-02-24 08:54:26 -0500

    Resolve typos
    Changing typos to initiate another CI process as the earlier CI failed for unknown reasons.

--- examples/vl53l0x_Interrupt/vl53l0x_Interrupt.ino
+++ examples/vl53l0x_Interrupt/vl53l0x_Interrupt.ino
@@ -1,7 +1,7 @@
 #include "Adafruit_VL53L0X.h"
-const byte VL53LOX_InterruptPin = 6;
-const byte VL53LOX_ShutdownPin = 9;
-volatile byte VL53LOX_State = LOW;
+const byte VL53L0X_InterruptPin = 6;
+const byte VL53L0X_ShutdownPin = 9;
+volatile byte VL53L0X_State = LOW;
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 void setup() {
@@ -13,23 +13,24 @@ void setup() {
   }
   Serial.println(F("VL53L0X API Interrupt Ranging example\n\n"));
 
-  pinMode(VL53LOX_ShutdownPin, OUTPUT);
-  pinMode(VL53LOX_ShutdownPin, INPUT_PULLUP);
-  pinMode(VL53LOX_InterruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(VL53LOX_InterruptPin), VL53LOXISR,
+  pinMode(VL53L0X_ShutdownPin, OUTPUT);
+  
+  pinMode(VL53L0X_ShutdownPin, INPUT_PULLUP);
+  pinMode(VL53L0X_InterruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(VL53L0X_InterruptPin), VL53L0XISR,
                   CHANGE);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
 
-  // if lox.begin failes its becasue it was a warm boot and the VL53LOX is in
-  // continues mesurement mode we can use an IO pin to reset the device in case
+  // if lox.begin fails its because it was a warm boot and the VL53L0X is in
+  // continues measurement mode we can use an IO pin to reset the device in case
   // we get stuck in this mode
   while (!lox.begin()) {
     Serial.println(F("Failed to boot VL53L0X"));
     Serial.println("Adafruit VL53L0X XShut set Low to Force HW Reset");
-    digitalWrite(VL53LOX_ShutdownPin, LOW);
+    digitalWrite(VL53L0X_ShutdownPin, LOW);
     delay(100);
-    digitalWrite(VL53LOX_ShutdownPin, HIGH);
+    digitalWrite(VL53L0X_ShutdownPin, HIGH);
     Serial.println("Adafruit VL53L0X XShut set high to Allow Boot");
     delay(100);
   }
@@ -46,14 +47,14 @@ void setup() {
                     VL53L0X_GPIOFUNCTIONALITY_THRESHOLD_CROSSED_LOW,
                     VL53L0X_INTERRUPTPOLARITY_LOW);
 
-  // Set Interrupt Treashholds
+  // Set Interrupt Thresholds
   // Low reading set to 50mm  High Set to 100mm
-  FixPoint1616_t LowThreashHold = (50 * 65536.0);
-  FixPoint1616_t HighThreashHold = (100 * 65536.0);
-  Serial.println("Set Interrupt Threasholds... ");
-  lox.setInterruptThresholds(LowThreashHold, HighThreashHold, true);
+  FixPoint1616_t LowThreshold = (50 * 65536.0);
+  FixPoint1616_t HighThreshold = (100 * 65536.0);
+  Serial.println("Set Interrupt Thresholds... ");
+  lox.setInterruptThresholds(LowThreshold, HighThreshold, true);
 
-  // Enable Continous Measurement Mode
+  // Enable Continuous Measurement Mode
   Serial.println("Set Mode VL53L0X_DEVICEMODE_CONTINUOUS_RANGING... ");
   lox.setDeviceMode(VL53L0X_DEVICEMODE_CONTINUOUS_RANGING, false);
 
@@ -61,16 +62,16 @@ void setup() {
   lox.startMeasurement();
 }
 
-void VL53LOXISR() {
+void VL53L0XISR() {
   // Read if we are high or low
-  VL53LOX_State = digitalRead(VL53LOX_InterruptPin);
+  VL53L0X_State = digitalRead(VL53L0X_InterruptPin);
   // set the built in LED to reflect in range on for Out of range off for in
   // range
-  digitalWrite(LED_BUILTIN, VL53LOX_State);
+  digitalWrite(LED_BUILTIN, VL53L0X_State);
 }
 
 void loop() {
-  if (VL53LOX_State == LOW) {
+  if (VL53L0X_State == LOW) {
     VL53L0X_RangingMeasurementData_t measure;
     Serial.print("Reading a measurement... ");
     lox.getRangingMeasurement(
